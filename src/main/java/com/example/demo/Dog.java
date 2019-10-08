d4f5675bbbd9d1c0a0cd95ba83f593c45e8081a4
GBTC406008UR <GBTC406008UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-08 09:41:02 -0400

    first commit

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Min(1)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Min(1)
+    private long age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+}
